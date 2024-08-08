e6fb97a5519cf1ca74e61296b94cfec2d881a70e
Gustavo <gustavohenriquealves418@gmail.com>
2024-08-08 18:58:07 -0300

    Conectando o bot

--- Bots/music_discord.py
+++ Bots/music_discord.py
@@ -3,4 +3,21 @@ import discord
 with open('key.txt', 'r') as key:
     key = key.read() 
 
+intents = discord.Intents.default() 
+intents.message_content = True 
 
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print("O bot {} está logando no servidor".format(client.user)) 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('Olá'):
+        await message.channel.send('Olá, como vai?!')
+
+client.run(key) 
